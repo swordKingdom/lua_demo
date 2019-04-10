5eb8cdfad711f67cf52457b7ff5295bb3a315410
yangjiajun <yangjiajun@kingsoft.com>
2019-04-10 14:48:37 +0800

    增加异常处理方面的代码

--- Coroutine.lua
+++ Coroutine.lua
@@ -18,6 +18,19 @@ print(co) -->thread: 线程编号
 
 --(1)查看协同程序的状态
 print(coroutine.status(co)) -->suspended
---(2)协同程序挂起
+--(2)协同程序由挂起状态变为运行态
 coroutine.resume(co)
 print(coroutine.status(co)) -->dead
+--(3)运行态转挂起
+co = coroutine.create(function ()
+    for i=1,10 do
+        print("co", i)
+        coroutine.yield()
+    end
+end)
+coroutine.resume(co) --> co 1
+print(coroutine.status(co)) --> suspended
+
+--Lua的协同称为不对称协同（asymmetric coroutines），指“挂起一个正在执行的协同函数”
+--与“使一个被挂起的协同再次执行的函数”是不同的，有些语言提供对称协同（symmetric
+--coroutines），即使用同一个函数负责“执行与挂起间的状态切换”
